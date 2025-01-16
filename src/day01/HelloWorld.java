1add1ab120997c504ee85f79e212acfda859c673
Armine0878 <162301700+Armine0878@users.noreply.github.com>
2025-01-16 17:54:55 +0400

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,5 +3,5 @@ package day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-    }
+        System.out.println("Hello Cdeo");    
 }
